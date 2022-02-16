294ff8bc9d128e1c0583c895be280850965520f5
Vladyslav <vladyslavromanenkoo@gmail.com>
2022-02-16 16:46:21 +0200

    First commit

---
+++ src/main/java/VAGService/VagServiceApplication.java
@@ -0,0 +1,13 @@
+package VAGService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VagServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VagServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/VAGService/VagServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package VAGService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VagServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
