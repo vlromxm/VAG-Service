ccc8ab56c79099c90231788cf3a16a119139f12f
Vladyslav <vladyslavromanenkoo@gmail.com>
2022-02-16 17:01:47 +0200

    ExampleController

---
+++ src/main/java/VAGService/controller/ExampleController.java
@@ -0,0 +1,12 @@
+package VAGService.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class ExampleController {
+
+    @GetMapping("/ping")
+    public String text(){
+        return "pong";
+    }
+}
