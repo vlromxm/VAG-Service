db9a94bc6f332a562d03cae1d0f3da23f0fe10e9
Vladyslav <vladyslavromanenkoo@gmail.com>
2022-02-21 23:55:12 +0200

    add /home page

--- src/main/java/VAGService/controllers/HomeController.java
+++ src/main/java/VAGService/controllers/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-    @GetMapping("/")
+    @GetMapping("/home")
     public String home(Model model) {
         model.addAttribute("title", "Главная страница");
         return "home";
